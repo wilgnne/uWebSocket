1a26aab3811a62296470f890b68a5a209d154ea3
Wilgnne <w.khawanbarbosa@gmail.com>
2020-04-10 22:08:41 -0300

    feat: Base da conexão com servidor websocket
    Adicão de eventos de abertura, erro e fechamento de conexão

---
+++ UnityClient/Assets/uWebSocket/ConnectionController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class ConnectionController : MonoBehaviour {
+    public static WebSocket ws { get; private set; }
+    public string url = "ws://localhost:3000";
+    void Awake () {
+        ws = new WebSocket (url);
+
+        ws.OnMessage += (sender, e) => {
+            Debug.Log ("Laputa says: " + e.Data);
+        };
+        ws.OnClose += (sender, e) => {
+            Debug.LogWarning ("uWebSocket Close: " + e.Reason);
+        };
+        ws.OnError += (sender, e) => {
+            Debug.LogError ("uWebSocket Error: " + e.ToString ());
+        };
+        ws.OnOpen += (sender, e) => {
+            Debug.Log ("uWebSocket Connection Open: " + e.ToString ());
+        };
+        
+        ws.OnOpen += (sender, e) => {
+            Debug.Log("Sending BALUS");
+            ws.Send("BALUS");
+        };
+
+        ws.Connect ();
+    }
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
