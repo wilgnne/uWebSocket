7d9d1e53011fc0e0844d0ecd75546cb4fd90e3ab
Wilgnne <w.khawanbarbosa@gmail.com>
2020-04-14 00:05:02 -0300

    feat: Inicio da implementalção de cliente de chat em tempo real

---
+++ UnityClient/Assets/uWebSocket/ChatController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatController : MonoBehaviour {
+    ConnectionController ws;
+
+    public GameObject loginPanel;
+    public InputField nameField;
+
+    public GameObject chatPanel;
+    public InputField messageField;
+
+    void Awake () {
+        ws = GameObject.FindGameObjectWithTag ("ServerController").GetComponent<ConnectionController> ();
+
+        ws.OnConnect ((sender, e) => {
+            Debug.Log ("Connected at server");
+            ws.Emit ("login", nameField.text);
+            loginPanel.SetActive (false);
+            chatPanel.SetActive (true);
+        });
+
+        ws.On("message", (sender, e) => {
+            
+        });
+    }
+
+    void Start () {
+        loginPanel.SetActive (true);
+        chatPanel.SetActive (false);
+    }
+
+    public void EntryButton () {
+        ws.Connect ();
+    }
+
+    public void SendButton () {
+        if (!String.IsNullOrEmpty (messageField.text) &&
+            !String.IsNullOrWhiteSpace (messageField.text)) {
+            ws.Emit("message", messageField.text);
+        }
+    }
+}
