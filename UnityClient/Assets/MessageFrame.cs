8144cd6dfc38600fa13fe7173811c239f36ac84a
Wilgnne <w.khawanbarbosa@gmail.com>
2020-04-19 00:02:39 -0300

    feat: Cliente da aplicação de chat

---
+++ UnityClient/Assets/MessageFrame.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageFrame : MonoBehaviour {
+    public Text nameFrame;
+    public Text messageFrame;
+}
--- UnityClient/Assets/uWebSocket/ChatController.cs
+++ UnityClient/Assets/uWebSocket/ChatController.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.UI;
 
+struct ChatMessage {
+    public string name;
+    public string message;
+
+    public ChatMessage (string _name, string _message) {
+        name = _name;
+        message = _message;
+    }
+}
+
 public class ChatController : MonoBehaviour {
     ConnectionController ws;
+
+    string nickname;
 
     public GameObject loginPanel;
     public InputField nameField;
@@ -13,18 +26,39 @@ public class ChatController : MonoBehaviour {
     public GameObject chatPanel;
     public InputField messageField;
 
+    public Transform content;
+    public GameObject messageFrame;
+
     void Awake () {
-        ws = GameObject.FindGameObjectWithTag ("ServerController").GetComponent<ConnectionController> ();
+        ws = GameObject.FindGameObjectWithTag ("ServerController").
+        GetComponent<ConnectionController> ();
 
         ws.OnConnect ((sender, e) => {
             Debug.Log ("Connected at server");
-            ws.Emit ("login", nameField.text);
+            ws.Emit ("login", nickname);
             loginPanel.SetActive (false);
             chatPanel.SetActive (true);
         });
 
-        ws.On("message", (data) => {
-            Debug.Log(data);
+        ws.On ("message", (data) => {
+            ChatMessage chatMessage = JsonConvert.DeserializeObject<ChatMessage> (data);
+
+            Vector3 framePos = new Vector3 (content.position.x,
+                content.position.y,
+                content.position.z);
+
+            GameObject goFrame = Instantiate (messageFrame,
+                framePos,
+                Quaternion.identity)
+            as GameObject;
+
+            goFrame.transform.SetParent (content);
+
+            MessageFrame mf = goFrame.GetComponent<MessageFrame> ();
+            mf.nameFrame.text = chatMessage.name;
+            mf.messageFrame.text = chatMessage.message;
+
+            messageField.text = "";
         });
     }
 
@@ -34,13 +68,15 @@ public class ChatController : MonoBehaviour {
     }
 
     public void EntryButton () {
+        nickname = nameField.text;
         ws.Connect ();
     }
 
     public void SendButton () {
+        Debug.Log ("Enviar mensagem: " + messageField.text);
         if (!String.IsNullOrEmpty (messageField.text) &&
             !String.IsNullOrWhiteSpace (messageField.text)) {
-            ws.Emit("message", messageField.text);
+            ws.Emit ("message", messageField.text);
         }
     }
 }
