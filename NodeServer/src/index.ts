112fd720d276c2769b7620acb156239879007a46
Wilgnne <w.khawanbarbosa@gmail.com>
2020-04-19 00:01:20 -0300

    feat: Aplicação de Chat

--- NodeServer/src/index.ts
+++ NodeServer/src/index.ts
@@ -2,10 +2,33 @@ import uServer from './uWebSocket';
 
 const uwss = new uServer({ port: 3000 });
 
+interface ChatMessage {
+  name: string;
+  message: string;
+}
+
 uwss.on('connection', (socket) => {
 
+  let userName: string;
+
   socket.on('login', (data) => {
-    console.log(data, " is connected!");
+    console.log("Cliente Logado");
+    userName = data;
+    const chatMessage: ChatMessage = {
+      name: "Server Info",
+      message: "O usuario " + userName + " acaba de entrar"
+    };
+
+    uwss.broadcast("message", chatMessage);
+  });
+
+  socket.on('message', (data) => {
+    const chatMessage: ChatMessage = {
+      name: userName,
+      message: data
+    };
+
+    uwss.broadcast('message', chatMessage);
   });
 
 });
